f7c53afd8efab99acf7bd8a83623d2bb37c12472
dsofadog <62201542+dsofadog@users.noreply.github.com>
2020-12-14 08:27:12 +0200

    Update plan.rb

--- plan.rb
+++ plan.rb
@@ -2,6 +2,10 @@ module Plan
   extend Hud::DSL
   extend self
   template "Layout", "screens/layout.mote"
-  template "Body", "screens/body.mote"
+  template "Body", "screens/body.mote" do
+    def body
+      "hello tushar"
+    end
+  end
   template "Title", "screens/title.mote"
 end
